4847bb3e551030705bb0813a5f0ef5736fbdb8cc
Amira-Saiid <58108079+Amira-Saiid@users.noreply.github.com>
2020-06-14 22:37:27 +0200

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,95 @@
+import VueRouter from "vue-router";
+import login from "./components/login.vue";
+import register from "./components/register.vue";
+import confirmation from "./components/confirmation.vue";
+import userDashboard from "./components/UserDashboard.vue";
+import api from "./api";
+import Toasted from "vue-toasted";
+import Vue from "vue";
+import NotifyApp from "./components/NotifyApp.vue";
+import UserHome from "./components/UserHome.vue";
+import NotifyOptions from './components/NotifyOptions.vue'
+import Currency from './components/Currency.vue';
+import Metals from './components/Metals.vue'
+Vue.use(Toasted, { duration: 2000 });
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+}
+const beforeEnterCheck = (to, from, next) => {
+  api
+    .checktoken()
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      Vue.toasted.error("Please login to continue");
+      next("/");
+    });
+};
+const beforeEnterLogin = async(to, from, next) => {
+  api
+    .checktoken()
+    .then(() => {
+      next("/home");
+    })
+    .catch(() => {
+      localStorage.removeItem('user-info');
+      next();
+    });
+};
+const beforeEnteroptions=async(to,from,next)=>{
+  if(to.params.price) return next();
+  next('/home/main')
+  
+}
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/home",
+      component: userDashboard,
+      beforeEnter: beforeEnterCheck,
+      children: [
+        {
+          path: "main",
+          name: "main",
+          component: NotifyApp,
+          
+        },
+        {
+          path: "options",
+          name: "options",
+          component: NotifyOptions,
+          beforeEnter:beforeEnteroptions,
+       
+          
+          
+        },
+        {
+          path:"currency",
+          name:"currency",
+          component:Currency,
+        },
+        {
+          path:"metals",
+          name:"metals",
+          component:Metals,
+        },
+        {
+          path: "",
+          name:"",
+          component: UserHome,
+         
+        },
+      ]
+    },
+    { path: "/confirmation", component: confirmation },
+    { path: "/register", component: register },
+    { path: "/", component: login, beforeEnter: beforeEnterLogin },
+    { path: "/login", component: login, beforeEnter: beforeEnterLogin }
+  ]
+});
+
+export default router;
